5b4076a5638770283ce917c7715bfeb729a28972
Tricarico1 <nathanieltricarico@gmail.com>
2025-03-22 08:02:50 -0400

    update
    Signed-off-by: Tricarico1 <nathanieltricarico@gmail.com>

--- two_pointer/move_zeros.py
+++ two_pointer/move_zeros.py
@@ -12,7 +12,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[nextNonZero], nums[i] = nums[i], nums[nextNonZero]
-                nextNonZero += 1
+                nextNonZero += 1 
 
                             
 
